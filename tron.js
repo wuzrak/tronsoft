15bb41959abfcdd64f2e2af5de2c5fe060f77a0a
wuzrak <wuzrak@gmail.com>
2017-05-16 12:09:32 +0200

    Add files via upload
    Animation that doesn't quite work right, I want it to put the divs in the center of the screen.

--- tron.js
+++ tron.js
@@ -1,6 +1,17 @@
+// $("#test").on("click", function(){
+//   $(this).toggleClass("clicked");
+// });
+
+$( document ).ready(function(){
+
+  $(".program").on("click", function(){
+    if($(".program").hasClass("clicked")){
+      $(".program").removeClass("clicked");
+    }else{
+        $(this).toggleClass("clicked");
+    }
+
+  });
 
 
-
-$(".icon").click(function(){
-  $('.nevidljiv').slideToggle();
-});
+})
